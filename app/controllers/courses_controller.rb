bed2a9d7569914e42aeef95f3bcdfa809c6ee878
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-29 17:06:40 +0000

    updated controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,38 +1,38 @@
-class CoursesController < ApplicationController 
+class CoursesController < ApplicationController
 
   get "/coucoursesrses" do
     redirect_if_not_logged_in
-    @bags = Course.all
+    @courses = Course.all
     erb :'golf_bags/index'
   end
 
   get "/courses/new" do
     redirect_if_not_logged_in
     @error_message = params[:error]
-    erb :'golf_bags/new'
+    erb :'courses/new'
   end
 
   get "/courses/:id/edit" do
     redirect_if_not_logged_in
     @error_message = params[:error]
-    @bag = Course.find(params[:id])
+    @course = Course.find(params[:id])
     erb :'courses/edit'
   end
 
   post "/bags/:id" do
     redirect_if_not_logged_in
-    @bag = Course.find(params[:id])
+    @course = Course.find(params[:id])
     unless Course.valid_params?(params)
       redirect "/courses/#{@bag.id}/edit?error=invalid golf bag"
     end
-    @bag.update(params.select{|k|k=="name" || k=="capacity"})
+    @course.update(params.select{|k|k=="name" || k=="capacity"})
     redirect "/courses/#{@bag.id}"
   end
 
   get "/courses/:id" do
     redirect_if_not_logged_in
-    @bag = Course.find(params[:id])
-    erb :'golf_bags/show'
+    @course = Course.find(params[:id])
+    erb :'courses/show'
   end
 
   post "/courses" do
