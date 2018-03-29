e18a1cb508d3889865a22f7668428fdbd953b89d
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-03-29 14:46:10 +0000

    updated table

--- db/migrate/20180329143958_create_courses.rb
+++ db/migrate/20180329143958_create_courses.rb
@@ -3,5 +3,7 @@ class CreateCourses < ActiveRecord::Migration[5.1]
     create_table :courses do |t|
       t.string :title
       t.string :department
+      t.string :location
+      t.string :professor
   end
 end
