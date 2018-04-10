2d4ba92da8e3211f9ed7e710bf07318f6fde870d
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-10 03:50:12 +0000

    updated validations

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,6 +1,7 @@
 class Semester < ActiveRecord::Base
   validates :season, :year, presence: true
   validates :year, length: { is: 4 }
+  validates :season, inclusion: { in: %w(Spring Summer Fall Winter)}
 
   belongs_to :user
   has_many :courses
